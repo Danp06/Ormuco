9b4047ba57f7f194dd251bcd0330891402ecb56b
Daniel Peña <99297786+Danp06@users.noreply.github.com>
2022-12-15 11:43:44 -0500

    Delete Ormuco directory

--- Ormuco/services/glance.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-port = '9292'
-
-def List_imagen(env_url,headers):
-   return requests.get(url=f"{env_url}:{port}/v2/images", headers=headers).json()['images']
-
-def List_imagen_id(env_url,headers,id):
-   return requests.get(url=f"{env_url}:{port}/v2/images/{id}", headers=headers).json()
